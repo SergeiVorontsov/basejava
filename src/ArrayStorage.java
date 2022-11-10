ef8d65a66f26122cf7caf6bf9212d73242133dbf
Sergey Vorontsov <vorontsov.s@mail.ru>
2022-11-10 17:07:54 +0300

    feat: implement methods for array based storage for Resume
    - add public static int variables for storage a quantity of existing Resumes, that used in operations with ArrayStorage;
    - implement "save" method by link assignment to the index of Array Storage equals the quantity of saved Resumes;
    - implement "get" method by cyclic search and comparing in ArrayStorage with the values of variable = "uuid";
    - implement "delete" method by searching index of deleted element, then copying a value (link) of the next index to previous till get "null";
    - implement "getAll" method by copying saved elements in array to temporary array;
    - implement "size" method by getting the value of variable = "elementCounter".

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,27 +4,54 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    public static int elementCounter = 0;
+
     void clear() {
+        for (int i = 0; i < elementCounter; i++) {
+            storage[i] = null;
+            elementCounter = 0;
+        }
+
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[elementCounter] = resume;
+        elementCounter++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume target = null;
+        for (Resume resume : storage) {
+            if (resume.toString().equals(uuid)) {
+                target = resume;
+                break;
+            }
+        }
+        return target;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                while (storage[i] != null && storage[i + 1] != null) {
+                    storage[i] = storage[++i];
+                }
+                elementCounter--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] all = new Resume[elementCounter];
+        System.arraycopy(storage, 0, all, 0, elementCounter);
+        return all;
     }
 
     int size() {
-        return 0;
+        return elementCounter;
     }
 }
