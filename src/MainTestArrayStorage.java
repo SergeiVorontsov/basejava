086359d3d9141af5755ef154c75f0c9a7bbd90bb
Sergey Vorontsov <vorontsov.s@mail.ru>
2022-11-14 16:31:51 +0300

    use update() in MainTestArrayStorage

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -26,6 +26,10 @@ public class MainTestArrayStorage {
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
+
+        r2.setUuid("uuid22");
+        ARRAY_STORAGE.update(r2);
+
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
