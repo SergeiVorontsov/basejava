ec9d5b40ec2c93d58420ddbdc399563c8ff7b204
Sergey Vorontsov <vorontsov.s@mail.ru>
2022-11-16 18:51:45 +0300

    refactor: get(), delete () with local value = index

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -8,20 +8,10 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-
     protected static final int STORAGE_LIMIT = 10000;
+
     private final Resume[] storage = new Resume[STORAGE_LIMIT];
-
     private int countResumes = 0;
-
-    private int getIndex(String uuid) {
-        for (int i = 0; i < countResumes; i++) {
-            if (storage[i].toString().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
     public void clear() {
         Arrays.fill(storage, 0, countResumes, null);
@@ -48,19 +38,21 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        if (getIndex(uuid) < 0) {
+        int index = getIndex(uuid);
+        if (index < 0) {
             System.err.println("ERROR Unable to get " + uuid + ": There is no such resume in database");
         } else {
-            return storage[getIndex(uuid)];
+            return storage[index];
         }
         return null;
     }
 
     public void delete(String uuid) {
-        if (getIndex(uuid) < 0) {
+        int index = getIndex(uuid);
+        if (index < 0) {
             System.err.println("ERROR Unable to delete " + uuid + ": There is no such resume in database");
         } else {
-            storage[getIndex(uuid)] = storage[countResumes - 1];
+            storage[index] = storage[countResumes - 1];
             storage[countResumes - 1] = null;
             countResumes--;
         }
@@ -75,5 +67,14 @@ public class ArrayStorage {
 
     public int size() {
         return countResumes;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < countResumes; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
