1f601664e2273f404781bdd039d8077a1c60b775
Sergey Vorontsov <vorontsov.s@mail.ru>
2022-11-20 14:56:56 +0300

    refactor: implement pattern template method, collect common methods

--- src/com/basejava/webapp/storage/AbstractArrayStorage.java
+++ src/com/basejava/webapp/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package com.basejava.webapp.storage;
 
 import com.basejava.webapp.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
 
@@ -10,6 +12,43 @@ public abstract class AbstractArrayStorage implements Storage {
 
     public int size() {
         return countResumes;
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, countResumes, null);
+        countResumes = 0;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, countResumes);
+    }
+
+    public void update(Resume resume) {
+        if (getIndex(resume.getUuid()) >= 0) {
+            storage[getIndex(resume.getUuid())] = resume;
+        } else {
+            System.err.println("ERROR Unable to update " + resume.getUuid() + ": There is no such resume in database");
+        }
+    }
+
+    public void save(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            System.err.println("ERROR Unable to save " + resume.getUuid() + ": Resume already exist in database");
+        } else if (STORAGE_LIMIT == countResumes) {
+            System.err.println("ERROR Unable to save " + resume.getUuid() + ": Resume database is full");
+        } else {
+            add(resume);
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.err.println("ERROR Unable to delete " + uuid + ": There is no such resume in database");
+        } else {
+            remove(index);
+        }
     }
 
     public Resume get(String uuid) {
@@ -21,6 +60,10 @@ public abstract class AbstractArrayStorage implements Storage {
         }
         return null;
     }
+
+    protected abstract void add(Resume resume);
+
+    protected abstract void remove(int index);
 
     protected abstract int getIndex(String uuid);
 }
