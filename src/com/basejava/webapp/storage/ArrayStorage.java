57644672fe208e968099442f2caf2e83f23e4ead
Sergey Vorontsov <vorontsov.s@mail.ru>
2022-11-20 14:59:57 +0300

    refactor: implement pattern template method, delete methods with common implementation

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -2,53 +2,25 @@ package com.basejava.webapp.storage;
 
 import com.basejava.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    public void clear() {
-        Arrays.fill(storage, 0, countResumes, null);
-        countResumes = 0;
-    }
-
-    public void update(Resume resume) {
-        if (getIndex(resume.getUuid()) >= 0) {
-            storage[getIndex(resume.getUuid())] = resume;
-        } else {
-            System.err.println("ERROR Unable to update " + resume.getUuid() + ": There is no such resume in database");
-        }
-    }
-
-    public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) >= 0) {
-            System.err.println("ERROR Unable to save " + resume.getUuid() + ": Resume already exist in database");
-        } else if (STORAGE_LIMIT == countResumes) {
-            System.err.println("ERROR Unable to save " + resume.getUuid() + ": Resume database is full");
-        } else {
-            storage[countResumes] = resume;
-            countResumes++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.err.println("ERROR Unable to delete " + uuid + ": There is no such resume in database");
-        } else {
-            storage[index] = storage[countResumes - 1];
-            storage[countResumes - 1] = null;
-            countResumes--;
-        }
-    }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, countResumes);
+
+    protected void add(Resume resume) {
+        storage[countResumes] = resume;
+        countResumes++;
+    }
+
+    @Override
+    protected void remove(int index) {
+        storage[index] = storage[countResumes - 1];
+        storage[countResumes - 1] = null;
+        countResumes--;
     }
 
     protected int getIndex(String uuid) {
