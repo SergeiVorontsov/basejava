707c3f4110fae3ebbe49d0bb9f551de8ebbf1999
Sergey Vorontsov <vorontsov.s@mail.ru>
2023-01-08 20:18:18 +0300

    refactor: AllStorageTest

--- test/com/basejava/webapp/storage/AllStorageTest.java
+++ test/com/basejava/webapp/storage/AllStorageTest.java
@@ -13,7 +13,8 @@ import org.junit.runners.Suite;
         StreamPathStorageTest.class,
         StreamFileStorageTest.class,
         XmlPathStorageTest.class,
-        JsonPathStorageTest.class
+        JsonPathStorageTest.class,
+        DataPathStorageTest.class
 })
 public class AllStorageTest {
 }
